19225c22dfb70c33bab3d51646129f1f9a2ab9b2
꧁༺җ_ĐāƦƙ_ŁēǤēƞĐ_җ༻꧂ <88062757+XDarkLegendX@users.noreply.github.com>
2021-10-31 19:42:21 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
-// jupterdb
-export { jupterDB, jupterDBSchema } from "./database/jupterDB";
+// reconDB
+export { reconDB, reconDBSchema } from "./database/reconDB";
+
+// giveaways
+export {
+    GiveawayClientOptions,
+    GiveawayClientSchema,
+    StartOptions,
+    GiveawayClient
+} from "./giveaways";
+
+// transcript
+export { generateTranscript, TranscriptOptions, Message } from "./transcripts";
+
+// pagination
+export {
+    Button,
+    ButtonNames,
+    PaginationOptions,
+    pagination
+} from "./pagination";
+
+// modmail
+export {
+    CloseMailSessionOptions,
+    ModMailClient,
+    ModMailModelOptions,
+    ModMailOptions
+} from "./modmail";
+
+// starboard
+export {
+    StarboardClient,
+    StarboardClientOptions,
+    StarboardGuild,
+    StarboardGuildOptions,
+    starMessageData
+} from "./starboard";
+
+// functions
+export { chatBot, ChatBotOptions } from "./functions/chatBot";
 
 // structures
 export {
-  Command,
-  CommandOptions,
-  RunFunction,
-  RunOptions,
+    Command,
+    CommandOptions,
+    RunFunction,
+    RunOptions
 } from "./structures/command";
