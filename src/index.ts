3af2cfcdac052b3d888524af6f5b96ab5140c249
꧁༺җ_ĐāƦƙ_ŁēǤēƞĐ_җ༻꧂ <88062757+XDarkLegendX@users.noreply.github.com>
2021-10-31 19:56:58 -0300

    v1.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
 // reconDB
 export { jupterDB, jupterDBSchema } from "./database/jupterDB";
 
+// giveaways
+export {
+    GiveawayClientOptions,
+    GiveawayClientSchema,
+    StartOptions,
+    GiveawayClient
+} from "./giveaways";
+
+// transcript
+export { generateTranscript, TranscriptOptions, Message } from "./transcripts";
+
+// pagination
+export {
+    Button,
+    ButtonNames,
+    PaginationOptions,
+    pagination
+} from "./pagination";
+
+// modmail
+export {
+    CloseMailSessionOptions,
+    ModMailClient,
+    ModMailModelOptions,
+    ModMailOptions
+} from "./modmail";
+
+// starboard
+export {
+    StarboardClient,
+    StarboardClientOptions,
+    StarboardGuild,
+    StarboardGuildOptions,
+    starMessageData
+} from "./starboard";
+
+// functions
+export { chatBot, ChatBotOptions } from "./functions/chatBot";
+
 // structures
 export {
     Command,
